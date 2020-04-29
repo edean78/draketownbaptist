2f42191e6f2d7af846e0abe134055848d37e57cd
edean78 <edean78@gmail.com>
2020-04-29 18:49:15 -0400

    created the pages for the website

---
+++ src/Pages/Missionaries.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Missionaries extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Missionaries
